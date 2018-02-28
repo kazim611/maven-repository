2705a75fbf5c5967568e7d0f62313f4270df4f31
Khan <kazim.khan@accenture.com>
2018-02-28 14:31:46 +0530

    fixed:test cases

--- src/test/java/com/mulesoft/test/HelloMavenTest.java
+++ src/test/java/com/mulesoft/test/HelloMavenTest.java
@@ -1,17 +1,21 @@
-/*package com.mulesoft.test;
+package com.mulesoft.test;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
 	@Test
     public void testHello() throws Exception{
-	runFlowAndExpect("mavenFlow", "HelloWorld");
+	MuleEvent event = runFlow("flights");
+    String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    assertEquals("application/json", contentType);
 	}
 	@Override
 	protected String getConfigFile(){
 	return "mule-project.xml";
 }
 }
-*/

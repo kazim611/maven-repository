69b955ae6a205b000f0e3d4581adec4ed21d736b
Khan <kazim.khan@accenture.com>
2018-02-28 11:35:37 +0530

    committed

---
+++ src/test/java/com/mulesoft/test/HelloMavenTest.java
@@ -0,0 +1,17 @@
+/*package com.mulesoft.test;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+	@Test
+    public void testHello() throws Exception{
+	runFlowAndExpect("mavenFlow", "HelloWorld");
+	}
+	@Override
+	protected String getConfigFile(){
+	return "mule-project.xml";
+}
+}
+*/
